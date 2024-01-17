8708bf425bdaeb271053bcec0c1ccf3d0f6abc11
Nyl1even <lgarzino30@gmail.com>
2024-01-17 14:24:20 +0100

    add crossed research and boxes for each ingredients

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import axios from 'axios';
 
 const NumberSelector = () => {
   const [selectedNumber, setSelectedNumber] = useState(1);
+  const [inputValues, setInputValues] = useState(Array.from({ length: 1 }, () => ""));
 
   const handleNumberChange = (e) => {
-    setSelectedNumber(parseInt(e.target.value, 10));
+    const newNumber = parseInt(e.target.value, 10);
+    setSelectedNumber(newNumber);
+    setInputValues(Array.from({ length: newNumber }, () => ""));
+  };
+
+  const handleInputChange = (index, value) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = value;
+    setInputValues(newInputValues);
   };
 
   return (
     <div>
-      <h1>Mille et un cocktails</h1>
-      <label>Sélectionnez le nombre d'ingrédients que vous avez sous la main :</label>
+      <label>Sélectionnez un nombre :</label>
       <select value={selectedNumber} onChange={handleNumberChange}>
         {[...Array(10)].map((_, index) => (
           <option key={index + 1} value={index + 1}>
@@ -19,12 +27,25 @@ const NumberSelector = () => {
           </option>
         ))}
       </select>
-
-      <p></p>
+      <label>
+      <input type="checkbox"></input>
+      Recherche croisée</label>
+      <div>
+        {[...Array(selectedNumber)].map((_, index) => (
+          <input
+            key={index}
+            type="text"
+            placeholder={`Zone de texte ${index + 1}`}
+            value={inputValues[index]}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default NumberSelector;
+
 
 
