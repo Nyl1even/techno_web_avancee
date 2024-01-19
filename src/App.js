553dcfeafaa6823cca32c769286dfd8c8d34487c
malurite <titouan.roussel13@gmail.com>
2024-01-19 20:23:33 +0100

    added autocomplete and styled what remained

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 const CocktailSearch = () => {
   const [ingredients, setIngredients] = useState([]);
+  const [glasses, setGlasses] = useState([]);
   const [inputValues, setInputValues] = useState(Array.from({ length: 1 }, () => ""));
   const [cocktails, setCocktails] = useState([]);
-  const [filteredIngredients, setFilteredIngredients] = useState([]);
   const [selectedOption, setSelectedOption] = useState('ingrédient');
-  const [selectedNumber, setSelectedNumber] = useState(1); // Add this line
+  const [filteredIngredients, setFilteredIngredients] = useState([]);
 
   useEffect(() => {
     const fetchIngredients = async () => {
@@ -19,29 +19,17 @@ const CocktailSearch = () => {
       }
     };
 
-    const handleRadioButtonChange = (e) => {
-      setSelectedOption(e.target.value);
-    };
-
-    const filterIngredients = ingredients.filter(
-      (ingredient) =>
-        ingredient.strIngredient1.toLowerCase().includes(value.toLowerCase())
-    );
-
-    // Mettre à jour filteredIngredients dans le composant parent
-    onInputChange(value, filterIngredients);
-
-    const searchCocktails = async () => {
-      let url = '';
-      if (selectedOption === 'ingrédient') {
-        url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
-      } else if (selectedOption === 'glass') {
-        url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list';
+    const fetchGlasses = async () => {
+      try {
+        const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list');
+        setGlasses(response.data.drinks);
+      } catch (error) {
+        console.error('Error fetching glasses:', error);
       }
-      // Rest of your fetch logic
-    };
+    }
 
     fetchIngredients();
+    fetchGlasses();
   }, []);
 
   const handleOptionChange = (e) => {
@@ -52,7 +40,6 @@ const CocktailSearch = () => {
     const newInputValues = [...inputValues];
     newInputValues[index] = value;
     setInputValues(newInputValues);
-
 
     const filterIngredients = ingredients.filter(
       (ingredient) =>
@@ -65,30 +52,31 @@ const CocktailSearch = () => {
   const searchCocktails = async () => {
     try {
       const ingredientsList = inputValues.filter(Boolean).join(',');
-      const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientsList}`);
+      let url = '';
+      if (selectedOption === 'ingrédient') {
+        url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientsList}`;
+      } else if (selectedOption === 'glass') {
+        url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${ingredientsList}`;
+      }
+      const response = await axios.get(url);
       setCocktails(response.data.drinks);
     } catch (error) {
       console.error('Error searching cocktails:', error);
     }
   };
 
-  // const handleNumberChange = (e) => {
-  //   const newNumber = parseInt(e.target.value, 10);
-  //   setSelectedNumber(newNumber);
-  //   setInputValues(Array.from({ length: newNumber }, () => ""));
-  // };
 
   return (
-    <div class='mainDiv'>
-      <h1 class="frontTitle">Cocktail Search</h1>
+    <div className='mainDiv'>
+      <h1 className="frontTitle">Cocktail Search</h1>
       <div className="numberSelectorDiv">
-        <label>Choisir entre un verre ou un ingrédient :</label>
+        <label id="intruct">Choisir entre un verre ou un ingrédient pour chercher un cocktail:</label>
         <div>
           <label>
             <input
               type="radio"
-              value="verre"
-              checked={selectedOption === "verre"}
+              value="glass"
+              checked={selectedOption === "glass"}
               onChange={handleOptionChange}
             />
             Verre
@@ -103,30 +91,37 @@ const CocktailSearch = () => {
             Ingrédient
           </label>
         </div>
-        {[...Array(selectedNumber)].map((_, index) => (
+        {inputValues.map((value, index) => (
           <div key={index}>
             <input
+              id="input"
               type="text"
-              placeholder={`Zone de texte ${index + 1}`}
-              value={inputValues[index]}
+              placeholder={selectedOption}
+              value={value}
               onChange={(e) => handleInputChange(index, e.target.value)}
+              list={`autocomplete-${index}`}
             />
             {selectedOption === "ingrédient" && ingredients && ingredients.length > 0 && (
-              <select value={inputValues[index]} onChange={(e) => handleInputChange(index, e.target.value)}>
-                {ingredients.map((ingredient) => (
-                  <option key={ingredient.strIngredient1} value={ingredient.strIngredient1}>
-                    {ingredient.strIngredient1}
-                  </option>
+              <datalist id={`autocomplete-${index}`}>
+                {filteredIngredients.map((ingredient) => (
+                  <option key={ingredient.strIngredient1} value={ingredient.strIngredient1} />
                 ))}
-              </select>
+              </datalist>
+            )}
+            {selectedOption === "glass" && glasses && glasses.length > 0 && (
+              <datalist id={`autocomplete-${index}`}>
+                {glasses.map((glass) => (
+                  <option key={glass.strGlass} value={glass.strGlass} />
+                ))}
+              </datalist>
             )}
           </div>
         ))}
       </div>
-      <button onClick={searchCocktails} class="button">Search Cocktails</button>
+      <button onClick={searchCocktails} className="button">Search Cocktails</button>
       <div>
-        <h2>Matching Cocktails:</h2>
-        <ul>
+        <h2 id="match">Matching Cocktails:</h2>
+        <ul id="matchList">
           {cocktails.map((cocktail) => (
             <li key={cocktail.idDrink}>{cocktail.strDrink}</li>
           ))}
